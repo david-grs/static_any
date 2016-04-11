6f909a327a3b1557b0c4b224401abc2a704592da
David G <david.a.grs@gmail.com>
2016-04-11 08:30:16 +0200

    Adding any_p with type check

--- any.hpp
+++ any.hpp
@@ -4,6 +4,7 @@
 #include <memory>
 #include <cstring>
 #include <type_traits>
+#include <typeinfo>
 
 namespace detail
 {
@@ -73,4 +74,84 @@ struct any : public detail::deleter<_DeleterT, any<_N>>
 
 private:
     std::array<char, _N> buff_;
+};
+
+template <std::size_t _N, typename _DeleterT = void>
+struct any_p : public detail::deleter<_DeleterT, any_p<_N>>
+{
+    typedef std::size_t size_type;
+
+    template <typename _T,
+              typename _D = _DeleterT>
+    typename std::enable_if<std::is_same<_D, void>::value, any_p&>::type
+    operator=(_T&& t)
+    {
+        static_assert(std::is_trivially_destructible<_T>::value, "_T is not trivially destructible and does not use any_p deleter");
+        copy_object(std::move(t));
+        return *this;
+    }
+
+    template <typename _T,
+              typename _D = _DeleterT>
+    typename std::enable_if<!std::is_same<_D, void>::value, any_p&>::type
+    operator=(_T&& t)
+    {
+        copy_object(std::move(t));
+        return *this;
+    }
+
+    template <typename _T>
+    void copy_object(_T&& t)
+    {
+        static_assert(std::is_trivially_copyable<_T>::value, "_T is not trivially copyable");
+        static_assert(size() >= sizeof(_T), "_T is too big to be copied to any_p");
+
+        std::memcpy(buff_.data(), (char*)&t, sizeof(_T));
+        type_ = typeid(_T).hash_code();
+    }
+
+    template <typename _T>
+    typename std::enable_if<!std::is_pointer<_T>::value, _T&>::type
+    get()
+    {
+        is_stored_type<_T>();
+        return reinterpret_cast<_T&>(*buff_.data());
+    }
+
+    template <typename _T>
+    typename std::enable_if<!std::is_pointer<_T>::value, const _T&>::type
+    get() const
+    {
+        is_stored_type<_T>();
+        return reinterpret_cast<const _T&>(*buff_.data());
+    }
+
+    template <typename _T>
+    typename std::enable_if<std::is_pointer<_T>::value, _T>::type
+    get()
+    {
+        is_stored_type<_T>();
+        return reinterpret_cast<_T>(buff_.data());
+    }
+
+    template <typename _T>
+    typename std::enable_if<std::is_pointer<_T>::value, const _T>::type
+    get() const
+    {
+        is_stored_type<_T>();
+        return reinterpret_cast<const _T>(buff_.data());
+    }
+
+    static constexpr size_type size() { return _N; }
+
+private:
+    template <typename _T>
+    void is_stored_type()
+    {
+        if (type_ != typeid(_T).hash_code())
+            throw std::bad_cast();
+    }
+
+    std::array<char, _N> buff_;
+    std::size_t          type_;
 };
