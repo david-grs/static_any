dc10849f5508c86a22eec0c475918c7e34ed5510
David G <david.a.grs@gmail.com>
2016-05-15 12:37:08 +0200

    Adding std::any to benchmark

--- benchmark.cpp
+++ benchmark.cpp
@@ -11,6 +11,8 @@ struct small_struct
     int i;
     void* v;
     double d;
+
+    int h() const { return i; }
 };
 
 Q_DECLARE_METATYPE(std::string)
@@ -50,6 +52,37 @@ int main()
         a = .2342;
     });
 
+    double d = .42;
+
+    QVariant qd = d;
+    boost::any bda = d;
+    std::experimental::any stdda = d;
+    static_any<8> sda = d;
+    static_any_t<8> stda = d;
+
+    int sum = 0;
+
+    s.add("qvariant get double", [&sum, &qd]()
+    {
+        sum += qd.toDouble();
+    });
+    s.add("boost.any get double", [&sum, &bda]()
+    {
+        sum += boost::any_cast<double>(bda);
+    });
+    s.add("std::any get double", [&sum, &stdda]()
+    {
+        sum += std::experimental::any_cast<double>(stdda);
+    });
+    s.add("static_any<8> get double", [&sum, &sda]()
+    {
+        sum += any_cast<double>(sda);
+    });
+    s.add("static_any_t<8> get double", [&sum, &stda]()
+    {
+        sum += stda.get<double>();
+    });
+
     s.add("qvariant small_struct assignment", []()
     {
         QVariant q;
@@ -76,34 +109,30 @@ int main()
         a = small_struct{2, nullptr, .45};
     });
 
-    QVariant q = 0xdeadbeef;
-    boost::any ba = 0xdeadbeef;
-    std::experimental::any stda = 0xdeadbeef;
-    static_any<8> sa = 0xdeadbeef;
-    static_any_t<8> sta = 0xdeadbeef;
+    small_struct sss{2, nullptr, .45};
+    QVariant qs;
+    qs.setValue(sss);
+    boost::any bas = sss;
+    std::experimental::any stdsa = sss;
+    static_any<32> ssa = sss;
 
-    int sum = 0;
+    s.add("qvariant get small_struct", [&sum, &qs]()
+    {
+        sum += qs.value<small_struct>().h();
+    });
+    s.add("boost.any get small_struct", [&sum, &bas]()
+    {
+        sum += boost::any_cast<small_struct>(bas).h();
+    });
+    s.add("std::any get small_struct", [&sum, &stdsa]()
+    {
+        sum += std::experimental::any_cast<small_struct>(stdsa).h();
+    });
+    s.add("static_any<32> get small_struct", [&sum, &ssa]()
+    {
+        sum += any_cast<small_struct>(ssa).h();
+    });
 
-    s.add("qvariant get uint", [&]()
-    {
-        sum += q.toUInt();
-    });
-    s.add("boost.any get uint", [&]()
-    {
-        sum += boost::any_cast<unsigned>(ba);
-    });
-    s.add("std::any get uint", [&]()
-    {
-        sum += std::experimental::any_cast<unsigned>(stda);
-    });
-    s.add("static_any<8> get uint", [&]()
-    {
-        sum += any_cast<unsigned>(sa);
-    });
-    s.add("static_any_t<8> get uint", [&]()
-    {
-        sum += sta.get<unsigned>();
-    });
 
     s.add("qvariant string assignment", []()
     {
@@ -132,19 +161,19 @@ int main()
     std::experimental::any astr = std::string("foobar");
     static_any<32> sstr = std::string("foobar");
 
-    s.add("qvariant get string", [&]()
+    s.add("qvariant get string", [&sum, &qstr]()
     {
         sum += qstr.value<std::string>().size();
     });
-    s.add("boost.any get string", [&]()
+    s.add("boost.any get string", [&sum, &bstr]()
     {
         sum += boost::any_cast<std::string>(bstr).size();
     });
-    s.add("std::any get string", [&]()
+    s.add("std::any get string", [&sum, &astr]()
     {
         sum += std::experimental::any_cast<std::string>(astr).size();
     });
-    s.add("static_any<32> get string", [&]()
+    s.add("static_any<32> get string", [&sum, &sstr]()
     {
         sum += any_cast<std::string>(sstr).size();
     });
